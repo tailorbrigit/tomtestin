b4522cec17fa18ca0baa4d3cbd768ed7162ca891
Szabó Brigitta <Herman Lszl>
2023-02-08 11:20:23 +0100

    componensek letrehozasa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
+import { TestindexComponent } from './testindex/testindex.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    GithubComponent,
+    TestindexComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/testindex/testindex.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestindexComponent } from './testindex.component';
+
+describe('TestindexComponent', () => {
+  let component: TestindexComponent;
+  let fixture: ComponentFixture<TestindexComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TestindexComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TestindexComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
