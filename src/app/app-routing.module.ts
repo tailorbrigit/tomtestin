ee1a082dac11d756d2ef3c9971a6c7a594aadb90
Szabó Brigitta <Herman Lszl>
2023-02-08 11:37:14 +0100

    about megirasa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
+import { TestindexComponent } from './testindex/testindex.component';
+import { GithubComponent } from './github/github.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {path: 'testindex', component: TestindexComponent},
